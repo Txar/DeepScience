0aa1c7731472ad853d312a0ffe2a31c5c8544de3
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-07-27 11:28:04 +0200

    dumb

--- scripts/blocks/stomper.js
+++ scripts/blocks/stomper.js
@@ -1,5 +1,5 @@
-const stomper = extendContent(PowerTurret, "stomper"){
+const stomper = extendContent(PowerTurret, "stomper", {
   updateShooting(tile){
     Damage.damage(tile.getTeam(),tile.drawx(),tile.drawy(),150,50);
   }
-}
+});
