16070443ac064e94cfec8dfac57e21636d03a5dc
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-08-10 19:52:32 +0200

    wow im really stupid

--- scripts/lum-crucible.js
+++ scripts/lum-crucible.js
@@ -1,8 +1,8 @@
 const lumcruc = extendContent(GenericSmelter, "lum-crucible", {
   draw(tile){
     this.super$draw();
-    Draw.color(liquids.current().color);
-    Draw.rect(Core.atlas.find(this.name + "-liquid");
+    Draw.color(tile.ent().liquids.current().color);
+    Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
     Draw.color();
    }
 });
