0f122eca18c07e6f16857909327960854f79e358
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-08-14 16:06:15 +0200

    Update prism.js

--- scripts/prism.js
+++ scripts/prism.js
@@ -1,4 +1,5 @@
 //Thx to GlennFolker
+//by ThePythonGuy <3
 function getLinkedBlock(tile, block, pred){
     entity = tile.ent();
     if(entity == null){return 0;}
@@ -10,18 +11,40 @@ function getLinkedBlock(tile, block, pred){
 const prism = extendContent(Block, "prism", {
 	load(){
 		this.super$load();
-		this.lightsource = Vars.content.getByName(ContentType.block, "deepscience-lightsource");
+		this.lightsource = Vars.content.getByName(ContentType.block, "deepscience-doom-light-source");
+		this.laserRegion = Core.atlas.find("laser");
+		this.laserEndRegion = Core.atlas.find("laser-end");
 	},
 	update(tile){
 		entity = tile.ent();
-		entity.setLinkedNum(getLinkedBlock(entity.tile, this.lightsource, boolf(tile => {return tile.block() == this.lightsource && tile.ent().cons.valid();})));
+		entity.setLinkedNum(getLinkedBlock(entity.tile, this.lightsource, boolf(tile => {return tile.block() == this.lightsource})));
+		var target = Units.closestTarget(tile.getTeam(), tile.drawx(), tile.drawy(), 300);
+		if(target!=null){
+			target.damage(entity.getLinkedNum());
+			Units.nearbyEnemies(tile.getTeam(), target.x-15, target.y-15, 30, 30, cons(e =>{
+				e.damage(entity.getLinkedNum()/2);
+			}));
+		}
 	},
 	draw(tile){
 		entity = tile.ent();
 		this.super$draw(tile);
-		Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()*entity,getLinkedNum());
-		Lines.square(tile.drawx(), tile.drawy(), 20+Mathf.sinDeg(Time.time()*2*entity.getLinkedNum())*5, (Time.time()+Mathf.sinDeg(Time.time()*2*entity.getLinkedNum())*5)*entity.getLinkedNum());
+		Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()*(entity.getLinkedNum()/2)));
+		Lines.square(tile.drawx(), tile.drawy(), 20+Mathf.sinDeg(Time.time()*2*entity.getLinkedNum())*5, Time.time()+(Time.time()+Mathf.sinDeg(Time.time()*2*entity.getLinkedNum())*5)*entity.getLinkedNum());
 		Draw.color();
+		var target = Units.closestTarget(tile.getTeam(), tile.drawx(), tile.drawy(), 300);
+		if(target!=null){
+			Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()*(entity.getLinkedNum()/2)));
+			Drawf.laser(this.laserRegion, this.laserEndRegion, tile.drawx(), tile.drawy(), target.getX(), target.getY(), target.health/target.maxHealth()+0.5);
+			Draw.color();
+			Units.nearbyEnemies(tile.getTeam(), target.x-20, target.y-20, 40, 40, cons(e =>{
+				if(e!=null){
+					Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()*(entity.getLinkedNum()/2)));
+					Drawf.laser(this.laserRegion, this.laserEndRegion, target.getX(), target.getY(), e.x, e.y, e.health/e.maxHealth()+0.1);
+					Draw.color();
+				}
+			}));
+		}
 	}
 });
 prism.entityType = prov(() => {
