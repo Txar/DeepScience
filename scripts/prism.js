a0549020eaec55d10faca5c0c0cee4822fe29fa2
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-08-14 16:21:52 +0200

    Make the lazor invisible if no lightsources are nearby

--- scripts/prism.js
+++ scripts/prism.js
@@ -18,12 +18,14 @@ const prism = extendContent(Block, "prism", {
 	update(tile){
 		entity = tile.ent();
 		entity.setLinkedNum(getLinkedBlock(entity.tile, this.lightsource, boolf(tile => {return tile.block() == this.lightsource})));
-		var target = Units.closestTarget(tile.getTeam(), tile.drawx(), tile.drawy(), 300);
-		if(target!=null){
-			target.damage(entity.getLinkedNum());
-			Units.nearbyEnemies(tile.getTeam(), target.x-15, target.y-15, 30, 30, cons(e =>{
-				e.damage(entity.getLinkedNum()/2);
-			}));
+		if(entity.getLinkedNum()>0){
+			var target = Units.closestTarget(tile.getTeam(), tile.drawx(), tile.drawy(), 300);
+			if(target!=null){
+				target.damage(entity.getLinkedNum());
+				Units.nearbyEnemies(tile.getTeam(), target.x-15, target.y-15, 30, 30, cons(e =>{
+					e.damage(entity.getLinkedNum()/2);
+				}));
+			}
 		}
 	},
 	draw(tile){
@@ -32,18 +34,20 @@ const prism = extendContent(Block, "prism", {
 		Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()*(entity.getLinkedNum()/2)));
 		Lines.square(tile.drawx(), tile.drawy(), 20+Mathf.sinDeg(Time.time()*2*entity.getLinkedNum())*5, Time.time()+(Time.time()+Mathf.sinDeg(Time.time()*2*entity.getLinkedNum())*5)*entity.getLinkedNum());
 		Draw.color();
-		var target = Units.closestTarget(tile.getTeam(), tile.drawx(), tile.drawy(), 300);
-		if(target!=null){
-			Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()*(entity.getLinkedNum()/2)));
-			Drawf.laser(this.laserRegion, this.laserEndRegion, tile.drawx(), tile.drawy(), target.getX(), target.getY(), target.health/target.maxHealth()+0.5);
-			Draw.color();
-			Units.nearbyEnemies(tile.getTeam(), target.x-20, target.y-20, 40, 40, cons(e =>{
-				if(e!=null){
-					Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()*(entity.getLinkedNum()/2)));
-					Drawf.laser(this.laserRegion, this.laserEndRegion, target.getX(), target.getY(), e.x, e.y, e.health/e.maxHealth()+0.1);
-					Draw.color();
-				}
-			}));
+		if(entity.getLinkedNum()>0){
+			var target = Units.closestTarget(tile.getTeam(), tile.drawx(), tile.drawy(), 300);
+			if(target!=null){
+				Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()*(entity.getLinkedNum()/2)));
+				Drawf.laser(this.laserRegion, this.laserEndRegion, tile.drawx(), tile.drawy(), target.getX(), target.getY(), target.health/target.maxHealth()+0.5);
+				Draw.color();
+				Units.nearbyEnemies(tile.getTeam(), target.x-20, target.y-20, 40, 40, cons(e =>{
+					if(e!=null){
+						Draw.color(Color.valueOf("ff0000").shiftHue(Time.time()*(entity.getLinkedNum()/2)));
+						Drawf.laser(this.laserRegion, this.laserEndRegion, target.getX(), target.getY(), e.x, e.y, e.health/e.maxHealth()+0.1);
+						Draw.color();
+					}
+				}));
+			}
 		}
 	},
 	drawPlace(x, y, rotation, valid){
