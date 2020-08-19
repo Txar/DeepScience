70af51ee9b503847d843cdfeb62b2246db9e90ca
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-08-19 14:10:00 +0200

    shdjkflsñd

--- scripts/stuffloader.js
+++ scripts/stuffloader.js
@@ -1,15 +1,15 @@
 print("Test [accent] TEST []");
-this.liquids = [];
-this.items = [];
+this.global.liquids = [];
+this.global.items = [];
 for(var i = 0; i < Vars.content.items().size; i++){
 	var item = Vars.content.items().get(i);
 	if(item == null) return;
-	this.items.push(item);
+	this.global.items.push(item);
 	var liquid = new Liquid(item.icon(Cicon.medium).name, item.color);
 	liquid.localizedName = "Molten " + item.localizedName;
 	liquid.flammability = 0.1;
 	liquid.temperature = 0.6;
 	liquid.heatCapacity = 0.3;
 	liquid.viscosity = 0.5;
-	this.liquids.push(liquid);
+	this.global.liquids.push(liquid);
 };
