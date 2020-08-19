56f377da14d7b1b23dfaab5475e50397e69c8245
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-08-19 17:16:05 +0200

    dumbass

--- scripts/melter.js
+++ scripts/melter.js
@@ -16,7 +16,7 @@ const melter = extendContent(Block, "melter", {
 			this.liquids.push(liquid);
 		};
 		for(var i = 0; i < Vars.content.liquids().size; i++){
-			var liquid = Vars.content.items.get(i);
+			var liquid = Vars.content.liquids().get(i);
 			if(liquid == null || liquid in this.liquids) return;
 			var item = new Item(liquid.icon(Cicon.medium).name, liquid.color);
 			item.localizedName = "Solid " + liquid.localizedName;
