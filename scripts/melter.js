cd04da0924ea29c91278f1f51dfb97e20a3a0e8c
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-08-19 21:28:12 +0200

    idk work please ig

--- scripts/melter.js
+++ scripts/melter.js
@@ -1,4 +1,4 @@
-const melter = extendContent(Block, "melter", {
+const melter = extendContent(GenericCrafter, "melter", {
 	load(){
 		this.super$load();
 		this.liquids = [];
@@ -15,12 +15,29 @@ const melter = extendContent(Block, "melter", {
 			liquid.viscosity = 0.5;
 			this.liquids.push(liquid);
 		};
-		for(var i = 0; i < Vars.content.liquids().size; i++){
-			var liquid = Vars.content.liquids().get(i);
-			if(liquid == null || liquid in this.liquids) return;
-			var item = new Item("solid-"+liquid.name, liquid.color);
-			item.localizedName = "Solid " + liquid.localizedName;
-			item.color = liquid.color;
-		};
-	}
+	},
+	update(tile){
+		entity = tile.ent();
+		if(entity.cons.valid()){
+            entity.progress += this.getProgressIncrease(entity, this.craftTime);
+            entity.totalProgress += entity.delta();
+           	entity.warmup = Mathf.lerpDelta(entity.warmup, 1, 0.02);
+			if(Mathf.chance(Time.delta() * this.updateEffectChance)){
+                Effects.effect(this.updateEffect, entity.x + Mathf.range(this.size * 4), entity.y + Mathf.range(this.size * 4));
+            }
+        }else{
+			entity.warmup = Mathf.lerp(entity.warmup, 0, 0.02);
+        }
+        if(entity.progress >= 1){
+            entity.cons.trigger();
+            if(entity.items.total()>0){
+            	entity.liquids.add(Vars.content.getByName(ContentType.liquid, entity.items.first().icon(Cicon.medium).name))
+            	this.tryDumpLiquid(tile, Vars.content.getByName(ContentType.liquid, entity.items.first().icon(Cicon.medium).name));
+            	entity.items.remove(entity.items.first(), 1);
+            }
+            Effects.effect(craftEffect, tile.drawx(), tile.drawy());
+            entity.progress = 0;
+        }
+    }
 });
+melter.meltTimer = melter.timers++;
